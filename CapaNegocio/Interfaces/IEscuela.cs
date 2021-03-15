fb1ab6a992d750c966e19ff9556a80db966e40ea
LosInjertosUAC <79822299+LosInjertosUAC@users.noreply.github.com>
2021-03-15 09:31:07 -0500

    Add files via upload

---
+++ CapaNegocio/Interfaces/IEscuela.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;//buffer de memoria
+namespace CapaNegocio.Interfaces
+{
+   interface IEscuela
+    {
+        DataSet Listar();
+
+        bool Agregar(string codEscuela, string escuela);
+
+        bool Eliminar(string codEscuela);
+
+        bool Actualizar(string codEscuela, string Escuela);
+
+        DataSet Buscar(string texto, string criterio);
+    }
+}
